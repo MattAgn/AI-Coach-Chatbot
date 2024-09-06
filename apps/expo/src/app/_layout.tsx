1fd424f401937515d09f39294fbd05fd816193bb
MattAgn <matthieua@bam.tech>
2024-09-06 22:00:13 +0200

    chore: clean some code

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -8,16 +8,10 @@ import { TRPCProvider } from "~/utils/api";
 
 import "../styles.css";
 
-// This is the main layout of the app
-// It wraps your pages with the providers they need
 export default function RootLayout() {
   const { colorScheme } = useColorScheme();
   return (
     <TRPCProvider>
-      {/*
-          The Stack component displays the current page.
-          It also allows you to configure your screens 
-        */}
       <Stack
         screenOptions={{
           headerStyle: {
