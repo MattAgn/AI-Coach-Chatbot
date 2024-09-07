1ec50208eb625ea3c594acee83f91a7832b567eb
MattAgn <matthieua@bam.tech>
2024-09-07 17:53:06 +0200

    feat: improve navigation

--- apps/expo/src/app/chat.tsx
+++ apps/expo/src/app/chat.tsx
@@ -1,4 +1,5 @@
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Stack } from "expo-router";
 import { Channel, MessageInput, MessageList } from "stream-chat-expo";
 
 import { useChat } from "./ChatContext";
@@ -11,6 +12,11 @@ export default function Chat() {
 
   return (
     <SafeAreaView className="bg-background">
+      <Stack.Screen
+        options={{
+          title: channel.data?.name ?? "Chat",
+        }}
+      />
       <Channel channel={channel}>
         <MessageList />
         <MessageInput />
--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -16,7 +16,12 @@ export default function Index() {
   return (
     <LinearGradient colors={["#667eea", "#764ba2"]} style={styles.container}>
       <SafeAreaView>
-        <Stack.Screen options={{ header: () => null }} />
+        <Stack.Screen
+          options={{
+            header: () => null,
+            title: "Home",
+          }}
+        />
         <View className="h-full w-full p-4">
           <Text style={styles.title}>Everyday wellness coaching</Text>
           <MyChats />
