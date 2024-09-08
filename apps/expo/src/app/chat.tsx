d9a53da420ff1da560256c6f6726699821a8d6d9
MattAgn <matthieua@bam.tech>
2024-09-08 16:50:37 +0200

    feat: make chatbot work for sleep

--- apps/expo/src/app/chat.tsx
+++ apps/expo/src/app/chat.tsx
@@ -1,11 +1,52 @@
+import { useEffect } from "react";
+import { Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
+import { Event, StreamChat } from "stream-chat";
 import { Channel, MessageInput, MessageList } from "stream-chat-expo";
 
+import { chatApiKey } from "~/chatConfig";
+import { api } from "~/utils/api";
 import { useChat } from "./ChatContext";
+
+const chatClient = StreamChat.getInstance(chatApiKey);
 
 export default function Chat() {
   const { channel } = useChat();
+
+  const respondToMessage = api.chatbot.getChatGptResponse.useMutation();
+
+  useEffect(() => {
+    const handleNewMessage = async (event: Event) => {
+      try {
+        const userRole = event?.message?.user?.role;
+        const channelType = event?.channel_type;
+        const channelId = event.channel_id;
+        const message = event?.message?.text;
+
+        if (
+          event.type !== "message.new" ||
+          userRole === "speaker" ||
+          !message ||
+          !channelType ||
+          !channelId
+        ) {
+          return;
+        }
+
+        respondToMessage.mutateAsync({ message });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    chatClient.on("message.new", handleNewMessage);
+
+    return () => {
+      chatClient.off("message.new", handleNewMessage);
+    };
+  }, [chatClient, respondToMessage]);
+
   if (!channel) {
     return null;
   }
--- packages/api/src/router/chatbot.ts
+++ packages/api/src/router/chatbot.ts
@@ -1,5 +1,6 @@
 import type { TRPCRouterRecord } from "@trpc/server";
 import OpenAI from "openai";
+import { StreamChat } from "stream-chat";
 import { z } from "zod";
 
 import { publicProcedure } from "../trpc";
@@ -7,6 +8,9 @@ import { publicProcedure } from "../trpc";
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+const STREAM_API_KEY = process.env.STREAM_API_KEY;
+const STREAM_SECRET = process.env.STREAM_SECRET;
 
 export const chatbotRouter = {
   getChatGptResponse: publicProcedure
@@ -17,9 +21,23 @@ export const chatbotRouter = {
         model: "gpt-4o",
       });
 
-      return {
-        botResponse:
-          chatCompletion.choices[0]?.message.content ?? "I have nothing to say",
+      const message =
+        chatCompletion.choices[0]?.message.content ??
+        "Sorry i'm asleep right now, come back later";
+
+      if (!STREAM_API_KEY || !STREAM_SECRET) {
+        throw new Error("STREAM_API_KEY and STREAM_SECRET are required");
+      }
+
+      const chatClient = StreamChat.getInstance(STREAM_API_KEY, STREAM_SECRET);
+
+      const channel = chatClient.channel("Chatgpt", "Sleep", {});
+
+      const gptmessage = {
+        text: message,
+        user_id: "sleep-ai-coach",
       };
+      channel.sendMessage(gptmessage);
+      return { botResponse: message };
     }),
 } satisfies TRPCRouterRecord;
