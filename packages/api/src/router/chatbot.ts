97d6103713c6f40a55b529b880814370af5803d7
MattAgn <matthieua@bam.tech>
2024-09-13 07:20:26 +0200

    feat: rename chat automatically

--- packages/api/src/router/chatbot.ts
+++ packages/api/src/router/chatbot.ts
@@ -24,6 +24,8 @@ const coachByChannel: Record<Category, string> = {
   [Category.Nutrition]: "nutrition-ai-coach",
   [Category.Sport]: "sport-ai-coach",
 };
+const DEFAULT_CHAT_NAME = "NO_NAME";
+const getLegeacyChatNamePrefix = (category: Category) => `${category}-`;
 
 export const chatbotRouter = {
   getChatGptResponse: publicProcedure
@@ -31,40 +33,74 @@ export const chatbotRouter = {
       z.object({ category: z.nativeEnum(Category), channelId: z.string() }),
     )
     .mutation(async ({ input }) => {
-      if (!STREAM_API_KEY || !STREAM_SECRET) {
-        throw new Error("STREAM_API_KEY and STREAM_SECRET are required");
+      try {
+        if (!STREAM_API_KEY || !STREAM_SECRET) {
+          throw new Error("STREAM_API_KEY and STREAM_SECRET are required");
+        }
+
+        const chatClient = StreamChat.getInstance(
+          STREAM_API_KEY,
+          STREAM_SECRET,
+        );
+
+        const channel = chatClient.channel(input.category, input.channelId);
+
+        const { messages: chatHistory } = await channel.query({
+          messages: { limit: 30 },
+        });
+
+        if (
+          channel.data?.name === DEFAULT_CHAT_NAME ||
+          channel.data?.name?.startsWith(
+            getLegeacyChatNamePrefix(input.category),
+          )
+        ) {
+          const chatNameCompletion = await openAiClient.chat.completions.create(
+            {
+              messages: [
+                {
+                  role: "system",
+                  content: getChatNamePrompt(
+                    chatHistory.map((message) => message.text ?? ""),
+                  ),
+                },
+              ],
+              model: "gpt-4o-mini",
+              temperature: 0.2,
+            },
+          );
+
+          const chatName =
+            chatNameCompletion.choices[0]?.message.content ?? "NO_NAME";
+
+          await channel.update({ name: chatName });
+        }
+
+        const chatCompletion = await openAiClient.chat.completions.create({
+          messages: [
+            {
+              role: "system",
+              content: getCoachingPrompt(input.category),
+            },
+            ...adaptStreamMessagesToGptMessages(chatHistory),
+          ],
+          model: "gpt-4o-mini",
+          temperature: 0.2,
+        });
+
+        const message =
+          chatCompletion.choices[0]?.message.content ??
+          "Sorry i'm asleep right now, come back later";
+        const gptmessage = {
+          text: message,
+          user_id: coachByChannel[input.category],
+        };
+        channel.sendMessage(gptmessage);
+        return { botResponse: message };
+      } catch (error) {
+        console.error(error);
+        throw new Error("Failed to get chat response");
       }
-
-      const chatClient = StreamChat.getInstance(STREAM_API_KEY, STREAM_SECRET);
-
-      const channel = chatClient.channel(input.category, input.channelId);
-
-      const { messages: chatHistory } = await channel.query({
-        messages: { limit: 30 },
-      });
-
-      const chatCompletion = await openAiClient.chat.completions.create({
-        messages: [
-          {
-            role: "system",
-            content: getCoachingPrompt(input.category),
-          },
-          ...adaptStreamMessagesToGptMessages(chatHistory),
-        ],
-        model: "gpt-4o",
-        temperature: 0.2,
-      });
-
-      const message =
-        chatCompletion.choices[0]?.message.content ??
-        "Sorry i'm asleep right now, come back later";
-
-      const gptmessage = {
-        text: message,
-        user_id: coachByChannel[input.category],
-      };
-
-      return { botResponse: message };
     }),
 } satisfies TRPCRouterRecord;
 
@@ -75,4 +111,16 @@ You asks questions first before giving concise solutions.
 You decline if you get asked questions on topics other than your expertise.
 You don't hesitate to say when you're not sure about something.
 Unless asked otherwise, don't reply with more than 6 sentences
+`;
+
+const getChatNamePrompt = (messages: string[]) => `
+  Based on the following messages that are part of a coaching chat, 
+  propose a short name for the chat (no more that 6 words) in the same language as the messages.
+  The name should be the main topic of the chat.
+  It should not contain the word chat as it is already implied.
+  Reply with ONLY the name of the chat, nothing else
+  If you can't come up with a suitable name due to lack of information, reply with "${DEFAULT_CHAT_NAME}".
+
+  The messages: 
+  ${messages.join("\n")}
 `;
