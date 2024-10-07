a35537c5ab94cf29b0dfe8c9a7b508e505b6beca
MattAgn <matthieua@bam.tech>
2024-10-07 07:51:25 +0200

    chore: change app name

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -2,13 +2,18 @@ import type { ConfigContext, ExpoConfig } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: "expo",
-  slug: "expo",
+  name: "ai_coaching",
+  slug: "ai_coaching",
   scheme: "expo",
   version: "0.1.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
+  extra: {
+    eas: {
+      projectId: "233c3376-15ce-4d42-b890-26e9d79c178d",
+    },
+  },
   splash: {
     image: "./assets/icon.png",
     resizeMode: "contain",
