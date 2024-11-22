4ac6532bf04d9fba8d1f5fe8774e55022787c4af
MattAgn <matthieua@bam.tech>
2024-11-22 23:00:49 +0100

    feat: display record button

--- apps/expo/src/app/(app)/mental-health.tsx
+++ apps/expo/src/app/(app)/mental-health.tsx
@@ -1,24 +1,57 @@
-import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import Animated, { FadeIn } from "react-native-reanimated";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 import { GradientBackground } from "~/view/components/GradientBackground";
 
 export default function MentalHealth() {
+  const [isRecording, setIsRecording] = useState(false);
+
+  const toggleRecording = () => {
+    if (isRecording) {
+      console.log("stop recording");
+    } else {
+      console.log("start recording");
+    }
+    setIsRecording((prev) => !prev);
+  };
+
   return (
     <GradientBackground>
       <SafeAreaView style={styles.container}>
         <Stack.Screen />
-        <View className="h-full w-full p-4">
+        <View style={{ flexGrow: 1 }}>
           <Animated.Text entering={FadeIn.duration(800)} style={styles.title}>
             Mental Health
           </Animated.Text>
+          <View
+            style={{
+              justifyContent: "center",
+              flexGrow: 1,
+            }}
+          >
+            <TouchableOpacity
+              onPress={toggleRecording}
+              style={styles.recordingButton}
+            >
+              <Ionicons
+                name={isRecording ? "square" : "mic"}
+                size={BUTTON_SIZE - 30}
+                color={isRecording ? "red" : "black"}
+                style={{ alignSelf: "center", justifyContent: "center" }}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
       </SafeAreaView>
     </GradientBackground>
   );
 }
+
+const BUTTON_SIZE = 90;
 
 const styles = StyleSheet.create({
   container: {
@@ -29,8 +62,14 @@ const styles = StyleSheet.create({
     fontSize: 23,
     textAlign: "center",
     fontWeight: "bold",
-    marginBottom: 30,
-    marginTop: 40,
     color: "white",
+  },
+  recordingButton: {
+    backgroundColor: "white",
+    borderRadius: BUTTON_SIZE / 2,
+    width: BUTTON_SIZE,
+    height: BUTTON_SIZE,
+    alignSelf: "center",
+    justifyContent: "center",
   },
 });
